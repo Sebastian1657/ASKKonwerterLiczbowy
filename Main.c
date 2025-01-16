2e3ea4bbca5c25bf8d500d86befd0f2107eff035
Sebastian1657 <miler.sebastian1101@gmail.com>
2025-01-16 01:59:35 +0100

    Konwersja int dec to bin
    Dodano podstawową konwersję liczby decymalnej na binarną i menu kontekstowe dla konwersji binarnych.

--- Main.c
+++ Main.c
@@ -1,21 +1,84 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
+#include <string.h>
+void menu();
 
-void PostacBajtowa(){
+
+void KonwersjaBinINT(){                     //dodać sprawdzenie czy liczba jest w zakresie intów, funkcjonalność dla liczb ujemnych, zabezpieczenie poprawności wprowadzanych danych
+    printf("Konwersja binarna INT\n");
+    int liczba, liczbacpy;
+    char liczbaBin[33]="";
+    char temp;
+    printf("Podaj liczbe: ");
+    scanf("%d", &liczba);
+    liczbacpy=liczba;
+    while(liczba>0){
+        if(liczba%2==1){
+            strcat(liczbaBin,"1");
+            liczba--;
+            liczba/=2;
+        }else{
+            strcat(liczbaBin,"0");
+            liczba/=2;
+        }
+    }
+    for(int i=strlen(liczbaBin)-1;i>=strlen(liczbaBin)/2;i--){
+        temp=liczbaBin[i];
+        liczbaBin[i]=liczbaBin[strlen(liczbaBin)-1-i];
+        liczbaBin[strlen(liczbaBin)-1-i]=temp;
+    }
+    printf("%d (dec) = %s (bin)\n",liczbacpy,liczbaBin);
+}
+void KonwersjaBinFLOAT(){
+    printf("Konwersja binarna FLOAT\n");
+}
+void KonwersjaBinDOUBLE(){
+    printf("Konwersja binarna DOUBLE\n");
+}
+void MenuPostacBinarna(){
+    char ch;
+    printf("+================================================+\n");
+    printf("I   Podaj typ danych, które chcesz wprowadzić:   I\n");
+    printf("I   1. INT                                       I\n");
+    printf("I   2. FLOAT                                     I\n");
+    printf("I   3. DOUBLE                                    I\n");
+    printf("I   4. Powrot do menu glownego                   I\n");
+    printf("I   5. Wyjdz z programu                          I\n");
+    printf("I                                                I\n");
+    printf("+================================================+\n");
+    do{
+        ch = getch();
+    }while(ch!='1'&&ch!='2'&&ch!='3'&&ch!='4'&&ch!='5'&&ch!=27);
+    switch(ch){
+        case '1':
+            system("cls");
+            KonwersjaBinINT();
+            break;
+        case '2':
+            system("cls");
+            KonwersjaBinFLOAT();
+            break;
+        case '3':
+            system("cls");
+            KonwersjaBinDOUBLE();
+            break;
+        case '4':
+            system("cls");
+            menu();
+            break;
+        case ('5'||27):
+            exit(0);
+    }
+};
+void MenuPostacBajtowa(){
     printf("Postac bajtowa danych\n");
-};
-void PostacBinarna(){
-    printf("Postac binarna danych\n");
-};
+}
 void OdejmowanieU2(){
     printf("Odejmowanie w U2\n");
 };
-void FloatToDec(){
+void FloatDec(){
     printf("Konwersja FLOAT to DEC\n");
-};
-void DecToFloat(){
-    printf("Konwersja DEC to FLOAT\n");
 };
 
 void menu(void){
@@ -25,22 +88,21 @@ void menu(void){
     printf("I   1. Drukuj postac bajtowa danych   I\n");
     printf("I   2. Drukuj postac binarna danych   I\n");
     printf("I   3. Zasady odejmowania w U2        I\n");
-    printf("I   4. Konwersja FLOAT to DEC         I\n");
-    printf("I   5. Konwersja DEC to FLOAT         I\n");
-    printf("I   6. Wyjdz z programu               I\n");
+    printf("I   4. Konwersja FLOAT <-> DEC        I\n");
+    printf("I   5. Wyjdz z programu               I\n");
     printf("I                                     I\n");
     printf("+=====================================+\n");
     do{
         ch = getch();
-    }while(ch!='1'&&ch!='2'&&ch!='3'&&ch!='4'&&ch!='5'&&ch!='6'&&ch!=27);
+    }while(ch!='1'&&ch!='2'&&ch!='3'&&ch!='4'&&ch!='5'&&ch!=27);
     switch(ch){
         case '1':
             system("cls");
-            PostacBajtowa();
+            MenuPostacBajtowa();
             break;
         case '2':
             system("cls");
-            PostacBinarna();
+            MenuPostacBinarna();
             break;
         case '3':
             system("cls");
@@ -48,13 +110,9 @@ void menu(void){
             break;
         case '4':
             system("cls");
-            FloatToDec();
+            FloatDec();
             break;
-        case '5':
-            system("cls");
-            DecToFloat();
-            break;
-        case ('6'||27):
+        case ('5'||27):
             exit(0);
     }
 };
